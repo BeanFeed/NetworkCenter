8d0acb09aa5784d6c2601ff6b343154f02493928
BeanFeed <two.austin.two@gmail.com>
2025-07-14 14:08:58 -0400

    add db

---
+++ DAL/Migrations/20250714174644_Add-SocketKeys.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSocketKeys : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SocketKey",
+                columns: table => new
+                {
+                    Key = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Scopes = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SocketKey", x => x.Key);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SocketKey");
+        }
+    }
+}
